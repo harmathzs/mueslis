9a7052b51e0e36b2b13439bcff3586972f6b6c5a
Zsolt Harmath <harmathzs@gmail.com>
2025-09-23 11:46:47 +0200

    Create MueslisPage

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+
+import MueslisPage from './pages/MueslisPage'
 import './App.css'
+
 
 export default class App extends React.Component {
   state = {
@@ -29,40 +32,10 @@ export default class App extends React.Component {
           </div>
         </nav>
 
-        <main id="content-area">
-          {/* Default content is Products/Prices table */}
-          <h1>My mueslis</h1>
-          <table>
-            <thead>
-              <tr>
-                <th>id</th>
-                <th>name</th>
-                <th>price</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.muesliData.result.map(muesli=><tr key={muesli.id}>
-                <td>{muesli.id}</td>
-                <td>{muesli.name}</td>
-                <td>{muesli.price}</td>
-              </tr>)}
-            </tbody>
-          </table>
-        </main>        
+        <MueslisPage />  
       </div>
     )
   }
 
-  async componentDidMount() {
-    try {
-      let muesliData = await fetch('http://localhost:3333/mueslis')
-      console.log('muesliData', muesliData)
-      muesliData = await muesliData.json()
-      console.log('muesliData', muesliData)
 
-      this.setState({muesliData})
-    } catch(e) {
-      console.warn(e)
-    }
-  }
 }
---
+++ react-app/src/pages/MueslisPage.jsx
@@ -0,0 +1,51 @@
+import React, {Component} from "react";
+
+
+
+export default class MueslisPage extends Component {
+  state = {
+    muesliData: {
+      result: []
+    }
+  }
+
+    render() {
+      return (
+        <div>
+            <main id="content-area">
+                {/* Default content is Products table */}
+                <h1>My mueslis</h1>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>name</th>
+                        <th>price</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.state.muesliData.result.map(muesli=><tr key={muesli.id}>
+                        <td>{muesli.id}</td>
+                        <td>{muesli.name}</td>
+                        <td>{muesli.price}</td>
+                    </tr>)}
+                    </tbody>
+                </table>
+            </main>          
+        </div>
+      )
+    }
+
+  async componentDidMount() {
+    try {
+      let muesliData = await fetch('http://localhost:3333/mueslis')
+      console.log('muesliData', muesliData)
+      muesliData = await muesliData.json()
+      console.log('muesliData', muesliData)
+
+      this.setState({muesliData})
+    } catch(e) {
+      console.warn(e)
+    }
+  }    
+}
